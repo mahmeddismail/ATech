95e30ca0487d592875003e68da82074345d7a718
Route <muhamedismail7777@gmail.com>
2023-12-01 19:47:20 +0200

    last

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -15,66 +15,20 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
 
   }
-
-
-  lines: any = [1, 2, 3, 4]
+copied:boolean=false;
+  copyToClipboard(text: string): void {
+    const tempInput = document.createElement('input');
+    tempInput.value = text;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand('copy');
+    document.body.removeChild(tempInput);
+    this.copied=true;
+    setTimeout(() => {
+    this.copied=false;
+      
+    }, 1500);
+    
+  }
 
 }
-// @ViewChild('myChart') myChart: ElementRef|undefined;
-// ngOnInit(): void {
-
-//   setTimeout(() => {
-//     this.userChart()
-//   }, 1500);
-
-// }
-
-
-// userChart(){
-//   const ctx = this.myChart?.nativeElement.getContext('2d');
-
-//   const myChart = new Chart(ctx, {
-//     type: 'line',
-//     data: {
-//       labels: ['ديسمبر', 'نوفمبر', 'اكتوبر', 'سبتمبر', 'اغسطس', 'يوليو', 'يونيو', 'مايو', 'ابريل', 'مارس', 'فبراير', 'يناير'],
-//       datasets: [{
-//         label: ' ',
-//         data: [65, 59, 80, 81, 56, 55, 40],
-//         fill: false,
-//         borderColor: '#8A74F9',
-//         tension: .4
-//       }]
-//     },
-//     options: {
-//       scales: {
-//         x: {
-//           display: true,
-//         },
-//         y: {
-//           display: false  ,
-//         }
-//       },
-//       plugins: {
-//         legend: {
-//           display: false,
-//         }
-//       },
-//       responsive: true,
-//       maintainAspectRatio: true,
-//       elements: {
-//         line: {
-//           borderWidth: 2,
-//           tension: 0.3, // Adjust the tension to make the line less smooth
-//           borderColor: 'rgba(75, 192, 192, 0.7)', // Add transparency for a faded look
-//         },
-//         point: {
-//           radius: 4,
-//           borderWidth: 2,
-//           backgroundColor: 'rgba(75, 192, 192, 0.7)',
-//           borderColor: 'white', // Add a white border around points
-//         },
-//       },
-//     }
-//   });
-// }
-
